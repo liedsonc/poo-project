00c628561a87aa5cd6a1a4c89ce1c21b242150bf
Liedson Correia <l.correia@thecodingmachine.com>
2025-06-12 19:32:55 +0100

    feat: enhance UNIAG class with new methods and initialization for entities

--- src/UNIAG.java
+++ src/UNIAG.java
@@ -14,55 +14,117 @@ public class UNIAG {
         this.investigadores = new ArrayList<>();
         this.invColaboradores = new ArrayList<>();
     }
-    // Métodos para adicionar entidades
-    public boolean addProjeto(int id, double fin) {
-        // FIXME: Can I add the title as well?
-        Projeto projeto = new Projeto(id, fin);
+
+    public ArrayList<Projeto> getProjetos() {
+        return projetos;
+    }
+    public ArrayList<Investigador> getInvestigadores() {
+        return investigadores;
+    }
+    public ArrayList<InvColaborador> getInvColaboradores() {
+        return invColaboradores;
+    }
+
+    public boolean addProjeto(int id, double fin, String titulo) {
+        Projeto projeto = new Projeto(id, fin, titulo);
         return this.projetos.add(projeto);
     }
+
     public boolean addInvestigador(int id, String nome) {
         Investigador investigador = new Investigador(id, nome);
         return this.investigadores.add(investigador);
     }
+
     public boolean addInvColaborador(int id, String nome) {
         InvColaborador invColaborador = new InvColaborador(id, nome);
         return this.invColaboradores.add(invColaborador);
     }
-    // FIXME: Does it make sanse to have a method to add visitantes?
-    // Since visitantes do not directly related to UNIAG
-//    public boolean addVisitante(int id, String nome) {
-//        Visitante visitante = new Visitante(id, nome);
-//        return this.
-//    }
+
+    public void addVisitante(int id, String nome) {
+        new Visitante(id, nome);
+    }
 
     public boolean associarMembroProjeto(int idMembro, int idProjeto) {
-        // Método para associar um membro a um projeto
-        // Verifica se o membro é um Investigador ou InvColaborador
-        for (Investigador investigador : investigadores) {
+        for(Investigador investigador : this.investigadores) {
             if (investigador.getId() == idMembro) {
-                for (Projeto projeto : projetos) {
+                for(Projeto projeto : this.projetos) {
                     if (projeto.getId() == idProjeto) {
                         return investigador.associarProjeto(projeto);
                     }
                 }
             }
         }
-        for (InvColaborador invColaborador : invColaboradores) {
+
+        for(InvColaborador invColaborador : this.invColaboradores) {
             if (invColaborador.getId() == idMembro) {
-                for (Projeto projeto : projetos) {
+                for(Projeto projeto : this.projetos) {
                     if (projeto.getId() == idProjeto) {
                         return invColaborador.associarProjeto(projeto);
                     }
                 }
             }
         }
+
         return false;
     }
 
     public boolean distribuirVerbaPorInvestigador(int idProj) {
-
-        return true;
+        Projeto projeto = findProjeto(idProj);
+        if (projeto != null) {
+            projeto.distribuirVerbaPorInvestigador();
+            return true;
+        }
+        return false;
     }
 
+    public void mostrarMembros() {
+        System.out.println("Investigadores:");
 
+        for(Investigador investigador : this.investigadores) {
+            investigador.print();
+        }
+
+        System.out.println("Investigadores Colaboradores:");
+
+        for(InvColaborador invColaborador : this.invColaboradores) {
+            invColaborador.print();
+        }
+
+    }
+
+    public Projeto findProjeto(int id) {
+        for(Projeto projeto : this.projetos) {
+            if (projeto.getId() == id) {
+                return projeto;
+            }
+        }
+
+        return null;
+    }
+
+    public Investigador findInvestigador(int id) {
+        for(Investigador investigador : this.investigadores) {
+            if (investigador.getId() == id) {
+                return investigador;
+            }
+        }
+
+        return null;
+    }
+
+    // Este metodo não está implementado corretamente, pois a classe UNIAG não tem uma lista de visitantes
+    // Mas esta presente na classe seguindo o Diagrama UML
+    public Visitante findVisitante(int id) {
+        return null;
+    }
+
+    public InvColaborador findInvColaborador(int id) {
+        for(InvColaborador invColaborador : this.invColaboradores) {
+            if (invColaborador.getId() == id) {
+                return invColaborador;
+            }
+        }
+
+        return null;
+    }
 }
