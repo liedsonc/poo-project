b247fe592979d40ff4d7e3c1e2a06c66a17b01af
Liedson Correia <l.correia@thecodingmachine.com>
2025-06-12 19:35:52 +0100

    feat: add getters, setters, and methods for saldo management in Investigador class

--- src/Investigador.java
+++ src/Investigador.java
@@ -9,4 +9,45 @@ public class Investigador extends MembroInvestigador{
         this.projetos = new ArrayList<>();
         this.saldo = 0.0;
     }
+
+    // Getters
+    public ArrayList<Projeto> getProjetos() {
+        return projetos;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    // Setters
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public boolean associarProjeto(Projeto p) {
+        if (!projetos.contains(p)) {
+            projetos.add(p);
+            p.associarInvestigador(this);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean adicionarInvSaldo(double val) {
+        if (val > 0) {
+            this.saldo += val;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void print() {
+        // metodo para imprimir os detalhes do investigador
+        System.out.println("Investigador: ");
+        System.out.println("ID: " + getId() + ", Nome: " + getNome() + ", Saldo: " + saldo);
+        System.out.println("Projetos associados:");
+        for (Projeto projeto : projetos) {
+            System.out.println(" - Projeto ID: " + projeto.getId() + ", Título: " + projeto.getTitulo());
+        }
+    }
 }
