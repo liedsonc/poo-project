90918a17c6ea394bad6bfdbeb268c2c9b42056be
Liedson Correia <l.correia@thecodingmachine.com>
2025-06-12 19:24:11 +0100

    feat: add print method to Visitante class

--- src/Visitante.java
+++ src/Visitante.java
@@ -2,4 +2,10 @@ public class Visitante extends MembroInvestigador{
     public Visitante(int id, String nome) {
         super(id, nome);
     }
+
+    @Override
+    public void print() {
+        // metodo para imprimir os detalhes do visitante
+        System.out.println("Visitante ID: " + getId() + ", Nome: " + getNome());
+    }
 }
